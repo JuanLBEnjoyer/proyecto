0efd583fd5f14655d76ded2a7b2e9112ec31efe4
juanpe23 <142370092+JuanLBEnjoyer@users.noreply.github.com>
2026-02-23 09:31:27 -0500

    Creacion de entidades y objetos de valor principales del proyecto

---
+++ src/main/java/co/edu/uniquindio/proyecto/domain/entity/Usuario.java
@@ -0,0 +1,21 @@
+package co.edu.uniquindio.proyecto.domain.entity;
+
+public class Usuario {
+
+
+
+    
+
+    public void registrarSolicitud(Solicitud solicitud) {
+
+    }
+
+    public void clasificarSolicitud(Solicitud solicitud) {
+
+    }
+
+    public void asignarResponsableSolicitud(Solicitud solicitud) {
+
+    }
+
+}
