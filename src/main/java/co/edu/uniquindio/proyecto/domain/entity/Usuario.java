964e6626a5c9384f502b0e31e7ade66cfe675cc9
Daniel Garcia <daniestiv89@gmail.com>
2026-03-05 10:08:55 -0500

    Se añadieron reglas de dominio para Usuario

--- src/main/java/co/edu/uniquindio/proyecto/domain/entity/Usuario.java
+++ src/main/java/co/edu/uniquindio/proyecto/domain/entity/Usuario.java
@@ -1,5 +1,6 @@
 package co.edu.uniquindio.proyecto.domain.entity;
 
+import co.edu.uniquindio.proyecto.domain.exception.ExcepcionDeReglaDeDominio;
 import co.edu.uniquindio.proyecto.domain.valueobject.*;
 
 import lombok.Getter;
@@ -13,6 +14,18 @@ public class Usuario {
     private RolUsuario rol;
 
     public Usuario(Documento documento, String nombre, Email email, RolUsuario rol) {
+        if (documento == null) {
+            throw new ExcepcionDeReglaDeDominio("El documento no puede ser nulo");
+        }
+        if (nombre == null || nombre.isBlank()) {
+            throw new ExcepcionDeReglaDeDominio("El nombre no puede estar vacio");
+        }
+        if (email == null) {
+            throw new ExcepcionDeReglaDeDominio("El email no puede ser nulo");
+        }
+        if (rol == null) {
+            throw new ExcepcionDeReglaDeDominio("El rol no puede ser nulo");
+        }
         this.documento = documento;
         this.nombre = nombre;
         this.email = email;
